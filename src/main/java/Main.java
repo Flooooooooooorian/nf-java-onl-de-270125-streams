5aea972d2dbdcd2cde22206f58a2d043b437923d
Flooooooooooorian <florian.weber@neuefische.de>
2025-02-12 14:51:05 +0100

    Add Stream

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Florian");
+        names.add("Martin");
+        names.add("Dominic");
+
+//        for (String name : names) {
+//            if(name.length() >= 7) {
+//                System.out.println(name);
+//            }
+//        }
+
+        List<Product> products = names.stream()
+                .filter(name -> name.length() >= 7)
+                .filter(name -> name.length() < 10)
+                .map(name -> new Product(String.valueOf(name.length()), name))
+//                .mapToInt(name -> name.length())
+                .collect(Collectors.toList());
+//                .forEach(name -> System.out.println(name));
+
+        System.out.println(products);
+    }
+}
